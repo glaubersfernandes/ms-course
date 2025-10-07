f22b031378820990753ffe11d9f15da0cce9e4a9
glauber.fernandes <glauber.fernandes@tbmtextil.com.br>
2025-10-07 00:41:06 -0300

    hr-worker implementation

--- hr-worker/src/main/java/br/com/gftecnologia/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/br/com/gftecnologia/hrworker/HrWorkerApplication.java
@@ -2,9 +2,17 @@ package br.com.gftecnologia.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class HrWorkerApplication {
+
+	@GetMapping("/")
+	String home() {
+		return "Bem-vindo ao hr-worker!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrWorkerApplication.class, args);
---
+++ hr-worker/src/main/java/br/com/gftecnologia/hrworker/resources/WorkerResource.java
@@ -0,0 +1,35 @@
+package br.com.gftecnologia.hrworker.resources;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.gftecnologia.hrworker.entities.Worker;
+import br.com.gftecnologia.hrworker.repositories.WorkerRepository;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerResource {
+
+	private WorkerRepository workerRepository;
+
+	public WorkerResource(WorkerRepository workerRepository) {
+		this.workerRepository = workerRepository;
+	}
+
+	@GetMapping
+	public ResponseEntity<List<Worker>> findAll() {
+		List<Worker> list = workerRepository.findAll();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		Worker obj = workerRepository.findById(id).get();
+		return ResponseEntity.ok(obj);
+	}
+}
