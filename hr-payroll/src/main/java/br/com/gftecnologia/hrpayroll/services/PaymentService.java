fcde1d62badf657066c36a05b371fdaffb5a7df3
glauber.fernandes <glauber.fernandes@tbmtextil.com.br>
2025-10-10 00:11:05 -0300

    RestTemplate

--- hr-payroll/src/main/java/br/com/gftecnologia/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/gftecnologia/hrpayroll/services/PaymentService.java
@@ -1,13 +1,32 @@
 package br.com.gftecnologia.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import br.com.gftecnologia.hrpayroll.entities.Payment;
+import br.com.gftecnologia.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
+	public RestTemplate restTemplate;
+
+	public PaymentService(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
 	public Payment getPayment(Long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", workerId.toString());
+
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
