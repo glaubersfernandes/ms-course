1e1afc23c95c540f3e1637e614bbee557c523ff8
glauber.fernandes <glauber.fernandes@tbmtextil.com.br>
2025-10-21 22:36:57 -0300

    Configuring Eureka server

--- hr-eureka-server/src/main/java/br/com/gftecnologia/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/br/com/gftecnologia/hreurekaserver/HrEurekaServerApplication.java
@@ -2,17 +2,11 @@ package br.com.gftecnologia.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@RestController
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
-
-	@GetMapping
-	String home() {
-		return "Bem vindo ao Eureka Server.";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrEurekaServerApplication.class, args);
